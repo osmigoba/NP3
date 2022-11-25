ffe23945fcbeff686059929112a8465fd17d389a
osmigoba <oscargon_13@hotmail.com>
2022-11-25 10:51:41 -0500

    first commit

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,11 @@
+    import React from 'react'
+    import './header.css';
+    const Header = () => {
+      return (
+        <div className='gpt3__header section__padding' id='home'>
+          <div className='gpt3__header-content'></div>
+        </div>
+      )
+    }
+    
+    export default Header
